3609a82df91a5622857ff927bad4be3687e88174
ORAU <heymandameunmail@gmail.com>
2023-05-19 12:14:00 -0600

    Assertions modifications step by step in PositiveTests class

--- src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -13,27 +13,51 @@ public class PositiveTests {
 	public void loginTest() {
 
 		/*
-		 * Steps Create driver Open Test page Enter username Enter password Click login
-		 * button Verificacion new url,logout button is visible,sucessful login message
+		 * Steps      
+		 * button ,logout ,sucessful login message
 		 */
 
 		System.setProperty("user.dir", "C:\\Users\\Orlan\\eclipse-workspace\\selenium-for-beginner");
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Orlan\\eclipse-workspace\\selenium-for-beginner\\src\\main\\resources\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/main/resources/chromedriver.exe");
+		//Create driver
 		WebDriver driver= new ChromeDriver();
+		//Open Test page
 		driver.get("https://the-internet.herokuapp.com/login");
 		driver.manage().window().maximize();
 		
+		//Enter username
 		WebElement username= driver.findElement(By.id("username"));
 		username.sendKeys("tomsmith");
+		//Enter password
 		WebElement password=driver.findElement(By.id("password"));
 		password.sendKeys("SuperSecretPassword!");
+		//Click login
 		WebElement loginBtn=driver.findElement(By.xpath("//button[@type='submit']"));
 		loginBtn.click();
+		
+		/* My version
 		String expectedUrl="https://the-internet.herokuapp.com/secure";
 		String resultUrl=driver.getCurrentUrl();
+		
 		WebElement message=driver.findElement(By.xpath("//div[@class='flash success' and contains(text(),'You logged into a secure area!')]"));
 		WebElement logoutBtn=driver.findElement(By.xpath("//a[@href='/logout']"));
 		Assert.assertTrue(expectedUrl.equals(resultUrl)&&logoutBtn.isDisplayed()&&message.isDisplayed(), "Passed Test");
+		*/
+		//Verificacion new url
+		String expectedUrl="https://the-internet.herokuapp.com/secure";
+		String resultUrl=driver.getCurrentUrl();
+		Assert.assertEquals(resultUrl, expectedUrl, "The current url is not as expected");
+		
+		//Button is visible
+		WebElement logoutBtn=driver.findElement(By.xpath("//a[@href='/logout']"));
+		Assert.assertTrue(logoutBtn.isDisplayed(), "The logout button is not displayed");
+		
+		
+		//sucessful login message
+		WebElement messageLabel=driver.findElement(By.xpath("//div[@class='flash success']"));
+		String actualMessage=messageLabel.getText();
+		String expectedMessage="You logged into a secure area!";
+		Assert.assertTrue(actualMessage.contains(expectedMessage), "Message is not as expected");
 		driver.quit();
 		
 	}

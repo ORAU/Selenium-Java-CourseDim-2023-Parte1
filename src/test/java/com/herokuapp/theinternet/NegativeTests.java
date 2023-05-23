e51e9b9c91890990a0e820e802588f1ceed25891
ORAU <heymandameunmail@gmail.com>
2023-05-23 14:26:16 -0600

    Se crea test de password incorrecto en clase negativeTests

--- src/test/java/com/herokuapp/theinternet/NegativeTests.java
+++ src/test/java/com/herokuapp/theinternet/NegativeTests.java
@@ -39,4 +39,34 @@ public void negativeUsernameTest() {
     //Close the browser
     driver.quit();
 	}
+	@Test
+	public void negativePassword() {
+		//Creating driver
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		String url="https://the-internet.herokuapp.com/login";
+		//Openning page
+		System.out.println("Openning page");
+		driver.manage().window().maximize();
+		driver.get(url);
+		//Writting correct username
+		System.out.println("Writting correct username");
+		WebElement username=driver.findElement(By.id("username"));
+		username.sendKeys("tomsmith");
+		//Writting incorrect password
+		System.out.println("Writting incorrect password");
+		WebElement password=driver.findElement(By.id("password"));
+		password.sendKeys("otherPassword");
+		//Clicking login button
+		System.out.println("Clicking login button");
+		WebElement loginBtn=driver.findElement(By.xpath("//button[@type='submit']"));
+		loginBtn.click();
+		String expectedMessage="Your password is invalid!";
+				
+		//Verifying results
+		System.out.println("Verifying results");		
+		String actualMessage=driver.findElement(By.id("flash")).getText();
+		Assert.assertTrue(actualMessage.contains(expectedMessage), "The actual message does not contain expected: \n Actual: "+actualMessage+" \n Expected:"+expectedMessage);
+		driver.quit();
+	}
 }

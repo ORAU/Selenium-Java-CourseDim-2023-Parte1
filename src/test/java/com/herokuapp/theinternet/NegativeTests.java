0bb51a87498018f0bd6919d6213de7d5857c6edc
ORAU <heymandameunmail@gmail.com>
2023-05-23 14:00:47 -0600

    Se agregan mensajes de salida por consola en cada paso del test de la clase NegativeTests

--- src/test/java/com/herokuapp/theinternet/NegativeTests.java
+++ src/test/java/com/herokuapp/theinternet/NegativeTests.java
@@ -9,27 +9,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class NegativeTests {
 	@Test
 public void negativeUsernameTest() {
+
 	//Create driver
 	System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 	WebDriver driver= new ChromeDriver();
 	String url="https://the-internet.herokuapp.com/login";
 	//Open page
+	System.out.println("Openning browser");
 	driver.manage().window().maximize();
 	driver.navigate().to(url);
 	//Enter incorrect username
 	WebElement username=driver.findElement(By.id("username"));
+	System.out.println("Writting username");
 	username.sendKeys("tommy");
 	//Enter correct password
 	WebElement password=driver.findElement(By.id("password"));
+	System.out.println("Writting password");
 	password.sendKeys("SuperSecretPassword!");
 	//Click on login button
 	WebElement loginBtn=driver.findElement(By.xpath("//button[@type='submit']"));
+	System.out.println("Clicking login button");
 	loginBtn.click();
 	//Verify validation message
 	WebElement alert=driver.findElement(By.xpath("//div[@id='flash']"));
 	String resultMessage=alert.getText();
 	String expectedMessage="Your username is invalid!";
-    Assert.assertTrue(resultMessage.contains(expectedMessage),"The result message after failed login is differente from expected");
+	System.out.println("Validating results");
+    Assert.assertTrue(resultMessage.contains(expectedMessage),"The result message after failed login is differente from expected. /n Actual Message: "+resultMessage+"/n Expected Message:"+expectedMessage);
     //Close the browser
     driver.quit();
 	}
